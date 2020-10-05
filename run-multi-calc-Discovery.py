9dc715b5beead5137c9b51bf52af754b23bef650
Ester <estercomellas@gmail.com>
2020-10-05 11:12:59 -0400

    Update run-multi-calc-Discovery.py for sensitivity study.

--- run-multi-calc-Discovery.py
+++ run-multi-calc-Discovery.py
@@ -28,30 +28,30 @@ for load in loads:
         # compute angles for radius and ulna loads
         radius_phi_min = str(50 - rom)
         radius_phi_max = str(50 + rom)
-        ulna_phi_min = str(23 - rom)
-        ulna_phi_max = str(23 + rom)
+        ulna_phi_min = str(23 + rom)
+        ulna_phi_max = str(23 - rom)
         
         for k in ks:
         
             # define kbio and kmech combinations
             if k==1:
-                kbio  = "1.0e-3"
-                kmech = "0.0"
+                kbio  = "0.0"
+                kmech = "1.0e-3"
                 
             elif k==2:
-                kbio  = "0.0"
+                kbio  = "1.0e-3"
                 kmech = "1.0e-3"
                                 
             elif k==3:
-                kbio  = "1.0e-3"
-                kmech = "1.0e-3"
-                                
-            elif k==4:
                 kbio  = "1.0e-2"
                 kmech = "1.0e-3"
                                 
-            else:
+            elif k==4:
                 kbio  = "1.0e-3"
+                kmech = "1.0e-2"
+                                
+            else:
+                kbio  = "1.0e-2"
                 kmech = "1.0e-2"
     
             # define jobname
@@ -93,3 +93,59 @@ for load in loads:
             
             # move back to upper folder
             os.chdir(cwd)
+
+
+                
+# Run one case with kmech = 0.0 (no need to study sensitivity of loading params)
+# Define parameters
+kbio  = "1.0e-3"
+kmech = "0.0"
+load = "-1850"
+rom = 20
+
+# compute angles for radius and ulna loads
+radius_phi_min = str(50 - rom)
+radius_phi_max = str(50 + rom)
+ulna_phi_min = str(23 + rom)
+ulna_phi_max = str(23 - rom)
+
+# define jobname
+jobname = "load_"+load+"__rom_"+str(rom)+"__kbio_"+kbio+"__kmech_"+kmech
+
+# create new folder, copy parameters and slurm files into it
+cwd = os.getcwd()
+os.mkdir(jobname)
+src_file_prm = cwd+"/parameters.prm"
+src_file_slurm = cwd+"/run-multi-dealii.slurm"
+dst_path = cwd+"/"+jobname
+shutil.copy2(src_file_prm, dst_path)
+shutil.copy2(src_file_slurm, dst_path)
+
+# access new folder and make changes in parameter file
+os.chdir(jobname)
+
+x = fileinput.input("parameters.prm", inplace=1)
+for line in x:
+    if "set Load value" in line:
+        line = "set Load value          = "+load+"\n"
+    if "set Radius phi min" in line:
+        line = "set Radius phi min   = "+radius_phi_min+"\n"
+    if "set Radius phi max" in line:
+        line = "set Radius phi max   = "+radius_phi_max+"\n"
+    if "set Ulna phi min" in line:
+        line = "set Ulna phi min   = "+ulna_phi_min+"\n"
+    if "set Ulna phi max" in line:
+        line = "set Ulna phi max   = "+ulna_phi_max+"\n"
+    if "set growth rate mech" in line:
+        line = "set growth rate mech = "+kmech+"\n"
+    if "set growth rate bio" in line:
+        line = "set growth rate bio = "+kbio+"\n"
+    print (line, end = ' '),
+x.close()
+
+# run problem
+os.system("sbatch run-multi-dealii.slurm")
+
+# move back to upper folder
+os.chdir(cwd)
+
