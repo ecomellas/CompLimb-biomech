d12a12b44c873e54b50cd08d152d1aac6e678fce
Ester <estercomellas@gmail.com>
2021-02-15 17:33:12 +0100

    Update parameters.prm and run-multi-calc-Discovery.py

--- run-multi-calc-Discovery.py
+++ run-multi-calc-Discovery.py
@@ -18,79 +18,62 @@ import time
 #-------------------------------------------------------------------------------
 # Modify parameters to study sensitivity
 #-------------------------------------------------------------------------------
-dts = ["1.0e-4","0.5e-4"]
-roms = [20,10]
-ks = [1,2,3,4,5]
+#dts = ["0.1","0.05"]
+loads = ["50e3","100e3"]
+ks = [1,2,3,4]
 
-for dt in dts:
-    for rom in roms:
-        
-        # compute angles for radius and ulna loads
-        radius_phi_min = str(50 - rom)
-        radius_phi_max = str(50 + rom)
-        ulna_phi_min = str(23 + rom)
-        ulna_phi_max = str(23 - rom)
-        
-        for k in ks:
-        
-            # define kbio and kmech combinations
-            if k==1:
-                kbio  = "1.0e-3"
-                kmech = "1.0e-2"
-                
-            elif k==2:
-                kbio  = "0"
-                kmech = "1.0e-2"
-                                
-            elif k==3:
-                kbio  = "1.0e-3"
-                kmech = "5.0e-3"
-                                
-            elif k==4:
-                kbio  = "1.0e-3"
-                kmech = "1.5e-2"
-                                
-            else:
-                kbio  = "0.1"
-                kmech = "1.0"
+#for dt in dts:
+for load in loads:
+    for k in ks:
     
-            # define jobname
-            jobname = "dt_"+dt+"__rom_"+str(rom)+"__kbio_"+kbio+"__kmech_"+kmech
-            
-            # create new folder, copy parameters and slurm files into it
-            cwd = os.getcwd()
-            os.mkdir(jobname)
-            src_file_prm = cwd+"/parameters.prm"
-            src_file_slurm = cwd+"/run-multi-dealii.slurm"
-            dst_path = cwd+"/"+jobname
-            shutil.copy2(src_file_prm, dst_path)
-            shutil.copy2(src_file_slurm, dst_path)
-            
-            # access new folder and make changes in parameter file
-            os.chdir(jobname)
-            
-            x = fileinput.input("parameters.prm", inplace=1)
-            for line in x:
-                if "set Radius phi min" in line:
-                    line = "set Radius phi min   = "+radius_phi_min+"\n"
-                if "set Radius phi max" in line:
-                    line = "set Radius phi max   = "+radius_phi_max+"\n"
-                if "set Ulna phi min" in line:
-                    line = "set Ulna phi min   = "+ulna_phi_min+"\n"
-                if "set Ulna phi max" in line:
-                    line = "set Ulna phi max   = "+ulna_phi_max+"\n"
-                if "set growth rate mech" in line:
-                    line = "set growth rate mech = "+kmech+"\n"
-                if "set growth rate bio" in line:
-                    line = "set growth rate bio = "+kbio+"\n"
-                if "set Time step size" in line:
-                    line = "set Time step size      = "+dt+"\n"
-                print (line, end = ' '),
-            x.close()
+        # define kbio and kmech combinations
+        if k==1:
+            kbio  = "1.0e-5"
+            kmech = "1.0"
+        
+        elif k==2:
+            kbio  = "1.0e-6"
+            kmech = "1.0"
+        
+        elif k==3:
+            kbio  = "1.0e-6"
+            kmech = "0.1"
+        
+        elif k==4:
+            kbio  = "1.0e-6"
+            kmech = "10.0"
 
-            # run problem
-            os.system("sbatch run-multi-dealii.slurm")
-            
-            # move back to upper folder
-            os.chdir(cwd)
+        # define jobname
+        jobname = "p-bio-func-orig__dt_"+dt+"__kbio_"+kbio+"__kmech_"+kmech+"__load_"+load
+        
+        # create new folder, copy parameters and slurm files into it
+        cwd = os.getcwd()
+        os.mkdir(jobname)
+        src_file_prm = cwd+"/parameters.prm"
+        src_file_slurm = cwd+"/run-multi-dealii.slurm"
+        dst_path = cwd+"/"+jobname
+        shutil.copy2(src_file_prm, dst_path)
+        shutil.copy2(src_file_slurm, dst_path)
+        
+        # access new folder and make changes in parameter file
+        os.chdir(jobname)
+        
+        x = fileinput.input("parameters.prm", inplace=1)
+        for line in x:
+            if "set Load value" in line:
+                line = "set Load value          = -"+load+"\n"
+            if "set growth rate mech" in line:
+                line = "set growth rate mech = "+kmech+"\n"
+            if "set growth rate bio" in line:
+                line = "set growth rate bio = "+kbio+"\n"
+#            if "set Time step size" in line:
+#                line = "set Time step size      = "+dt+"\n"
+            print (line, end = ' '),
+        x.close()
+
+        # run problem
+        os.system("sbatch run-multi-dealii.slurm")
+        
+        # move back to upper folder
+        os.chdir(cwd)
 
